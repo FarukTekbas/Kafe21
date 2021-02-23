1c75753899d44c34f49913c56150141667ab34cd
Faruk Tekbas <faruktekbas60@gmail.com>
2021-02-23 18:15:15 +0300

    ef6 entegrasyon

--- Kafe21.Data/KafeVeri.cs
+++ Kafe21.Data/KafeVeri.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Kafe21.Data
 {
-    public class KafeVeri
+    public class KafeVeri : DbContext
     {
+        public KafeVeri() : base("name=KafeVeri")
+        {
+
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .Entity<SiparisDetay>()//Siparis Detay entitysinin
+                .HasRequired(x => x.Urun) // Zorunlu olarak Urun u vardır.
+                .WithMany(x => x.SiparisDetaylar)// Ki bu Urun  birden cok siparis Detay' da bulunabilir
+                .HasForeignKey(x => x.UrunId)// SiparisDetay'dan Urun'e referans veren FK' sı UrunId alanıdır
+                .WillCascadeOnDelete(false);// urun silinirse bağlı olduğu Siparis Detyaı otomatik olarak silme
+        }
         public int MasaAdet { get; set; } = 20;
-        public List<Urun> Urunler { get; set; } = new List<Urun>();
-        public List<Siparis> AktifSiparisler { get; set; } = new List<Siparis>();
-        public List<Siparis> GecmisSiparisler { get; set; } = new List<Siparis>();
+        public DbSet<Urun> Urunler { get; set; }
+        public DbSet<Siparis> Siparisler { get; set; }
+        public DbSet<SiparisDetay> SiparisDetaylar { get; set; }
 
     }
 }
